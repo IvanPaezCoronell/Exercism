05884baa243f8957885a1290fb9b0165f39b4728
Ivan Paez Coronell <ipaez1@cuc.edu.co>
2021-10-04 20:53:14 -0500

    Resistor Color Duo

---
+++ resistor-color-duo/resistor_color_duo.py
@@ -0,0 +1,17 @@
+dic_colors = {
+             "black": 0,
+             "brown": 1,
+             "red": 2,
+             "orange": 3,
+             "yellow": 4,
+             "green": 5,
+             "blue": 6,
+             "violet": 7,
+             "grey": 8,
+             "white": 9,
+            }
+
+
+def value(colors):
+    dig = int(str(dic_colors[colors[0]]) + str(dic_colors[colors[1]]))
+    return dig
